a01f7ae48c43bd9d58c604abaafff7cef79e79db
Jordan Holmes <jordanstholmes@gmail.com>
2020-12-01 11:51:42 -0800

    Remove bundle from git tracking

--- dist/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{var t=new function(t,o,a){this.firstName=t,this.middleInitial="S.",this.lastName=a,this.fullName="Jordan S. Holmes"}("Jordan","S.","Holmes");document.body.textContent="Hello Mr. "+t.lastName+", how may I do your bidding today?"})();
