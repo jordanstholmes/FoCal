109e30a068c5c70c46ca0b1580e7c1ad349b7cb1
Jordan Holmes <jordanstholmes@gmail.com>
2020-12-01 11:00:20 -0800

    Add basic setup with typescript tutorial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/sup', (req, res) => {
+  res.send('sup, jordan');
+});
+
+app.listen(PORT, () => {
+   console.log('Sever listening at port ' + PORT);
+});
